87742bd8af98675f1ed9c45b3608c23c39022412
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2025-07-23 21:37:36 +0400

    added class MultimodalSentimentModel

--- models.py
+++ models.py
@@ -76,3 +76,60 @@ class AudioEncoder(nn.Module):
         #Features output : [batch_size, 128, 1]
 
         return self.projection(features.squeeze(-1))
+    
+class MultimodalSentimentModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        #Encoders
+        self.text_encoder = TextEncoder()
+        self.video_encoder = VideoEncoder()
+        self.audio_encoder = AudioEncoder()
+
+        #Fusion layer
+        self.fusion_layer = nn.Sequential(
+            nn.Linear(128 * 3, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.3)
+        )
+
+        #Classification heads
+        self.emotion_classifier = nn.Sequential(
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(64, 7) # Sadness, anger etc
+        )
+
+        self.sentiment_classifier = nn.Sequential(
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2,),
+            nn.Linear(64, 3) #negative, positive, neutral
+        )
+
+    def forward(self, text_inputs, video_frames, audio_features):
+        text_features = self.text_encoder(
+            text_inputs['input_ids'],
+            text_inputs['attention_mask']
+        )
+        video_features = self.video_encoder(video_frames)
+        audio_features = self.audio_encoder(audio_features)
+
+        # Concatenate multimodal features
+        combined_features = torch.cat([
+            text_features,
+            video_features,
+            audio_features
+        ], dim=1) # [batch_size, 128 * 3]
+
+        fused_features = self.fusion_layer(combined_features)
+
+        emotion_output = self.emotion_classifier(fused_features)
+        sentiment_output = self.sentiment_classifier(fused_features)
+
+        return {
+            'emotions': emotion_output,
+            'sentiments': sentiment_output
+        }
