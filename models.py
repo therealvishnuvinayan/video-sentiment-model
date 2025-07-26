c3a30fd7dfb6af4dd239822c71bd8c32c72e6e8c
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2025-07-26 12:25:46 +0400

    Printing Predictions for utterance

--- models.py
+++ models.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 from transformers import BertModel
 from torchvision import models as vision_models
-
+from meld_dataset import MELDDataset
 
 class TextEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.bert = BertModel.from_pretrained('bert_base-uncased')
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
 
         for param in self.bert.parameters():
             param.requires_grad = False
@@ -45,16 +45,16 @@ class VideoEncoder(nn.Module):
         return self.backbone(x)
 
 
-class AudioEncoder(nn.Module): 
+class AudioEncoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv_layers = nn.Sequential(
-            #Lower level features
+            # Lower level features
             nn.Conv1d(64, 64, kernel_size=3),
             nn.BatchNorm1d(64),
             nn.ReLU(),
             nn.MaxPool1d(2),
-            #Higher level features
+            # Higher level features
             nn.Conv1d(64, 128, kernel_size=3),
             nn.BatchNorm1d(128),
             nn.ReLU(),
@@ -69,24 +69,25 @@ class AudioEncoder(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.2)
         )
-        
+
     def forward(self, x):
         x = x.squeeze(1)
         features = self.conv_layers(x)
-        #Features output : [batch_size, 128, 1]
+        # Features output : [batch_size, 128, 1]
 
         return self.projection(features.squeeze(-1))
-    
+
+
 class MultimodalSentimentModel(nn.Module):
     def __init__(self):
         super().__init__()
 
-        #Encoders
+        # Encoders
         self.text_encoder = TextEncoder()
         self.video_encoder = VideoEncoder()
         self.audio_encoder = AudioEncoder()
 
-        #Fusion layer
+        # Fusion layer
         self.fusion_layer = nn.Sequential(
             nn.Linear(128 * 3, 256),
             nn.BatchNorm1d(256),
@@ -94,19 +95,19 @@ class MultimodalSentimentModel(nn.Module):
             nn.Dropout(0.3)
         )
 
-        #Classification heads
+        # Classification heads
         self.emotion_classifier = nn.Sequential(
             nn.Linear(256, 64),
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.Linear(64, 7) # Sadness, anger etc
+            nn.Linear(64, 7)  # Sadness, anger etc
         )
 
         self.sentiment_classifier = nn.Sequential(
             nn.Linear(256, 64),
             nn.ReLU(),
             nn.Dropout(0.2,),
-            nn.Linear(64, 3) #negative, positive, neutral
+            nn.Linear(64, 3)  # negative, positive, neutral
         )
 
     def forward(self, text_inputs, video_frames, audio_features):
@@ -122,7 +123,7 @@ class MultimodalSentimentModel(nn.Module):
             text_features,
             video_features,
             audio_features
-        ], dim=1) # [batch_size, 128 * 3]
+        ], dim=1)  # [batch_size, 128 * 3]
 
         fused_features = self.fusion_layer(combined_features)
 
@@ -133,3 +134,43 @@ class MultimodalSentimentModel(nn.Module):
             'emotions': emotion_output,
             'sentiments': sentiment_output
         }
+
+
+if __name__ == '__main__':
+    dataset = MELDDataset(
+        '../dataset/train/train_sent_emo.csv', '../dataset/train/train_splits')
+
+    sample = dataset[0]
+
+    model = MultimodalSentimentModel()
+    model.eval()
+
+    text_inputs = {
+        'input_ids': sample['text_inputs']['input_ids'].unsqueeze(0),
+        'attention_mask': sample['text_inputs']['attention_mask'].unsqueeze(0)
+    }
+
+    video_frames = sample['video_frames'].unsqueeze(0)
+    audio_features = sample['audio_features'].unsqueeze(0)
+
+    with torch.inference_mode():
+        outputs = model(text_inputs, video_frames, audio_features)
+
+        emotion_probs = torch.softmax(outputs['emotions'], dim=1)[0]
+        sentiment_probs = torch.softmax(outputs['sentiments'], dim=1)[0]
+
+    emotion_map = {
+        0: 'anger', 1: 'disgust', 2: 'fear', 3: 'joy', 4: 'neutral', 5: 'sadness', 6: 'surprise'
+    }
+
+    sentiment_map = {
+        0: 'negative', 1: 'neutral', 2: 'positive'
+    }
+
+    for i, prob in enumerate(emotion_probs):
+        print(f'{emotion_map[i]}: {prob:.2f}')
+
+    for i, prob in enumerate(sentiment_probs):
+        print(f'{sentiment_map[i]}: {prob:.2f}')
+
+    print('Predictions for utterance')
